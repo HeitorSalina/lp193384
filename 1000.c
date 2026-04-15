1c840dd90b656c208ce63a80b7ab44c5ac88847d
HeitorSalina <heitor.salina@unifesp.br>
2026-04-15 20:45:58 -0300

    Hello World
    Added header comments with exercise details.

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 2758
+Data        : 08/04/2026
+Nome        : Heitor Felipe de Goes Salina
+Objetivo    : Escrever Hello World!.
+Aprendizado : Imprimir alguma mensagem na saída.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
